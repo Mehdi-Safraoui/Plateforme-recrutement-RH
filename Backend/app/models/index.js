f773a9a58676d04258053f159dfb07f03b8c36c3
Koussaybejaoui <115742315+Koussaybejaoui@users.noreply.github.com>
2024-04-25 19:04:18 +0200

    Update index.js

--- Backend/app/models/index.js
+++ Backend/app/models/index.js
@@ -24,6 +24,7 @@ db.sequelize = sequelize;
 
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.role = require("../models/role.model.js")(sequelize, Sequelize);
+db.emploi = require("../models/emploi.model.js")(sequelize, Sequelize);
 
 db.role.belongsToMany(db.user, {
   through: "user_roles"
